0b551d2395dcee82b8b220188bcf0fc597d4cb76
liqing.zhan <liqing.zhan@qunar.com>
2016-07-12 18:48:00 +0800

    dd

---
+++ src/main/java/com/zlq/current/LongAdderTest.java
@@ -0,0 +1,28 @@
+package com.zlq.current;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Created by liqing.zhan on 2016/7/12.
+ */
+public class LongAdderTest {
+    @Test
+    public void t1() throws InterruptedException {
+        LongAdder longAdder = new LongAdder();
+        int count = 5;
+        while (count-- > 0) {
+            new Thread(() -> {
+                int end = 100;
+                while (end-- > 0) {
+                    longAdder.add(1);
+                }
+            }).start();
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(longAdder.longValue());
+    }
+}
--- src/main/java/com/zlq/current/LongTypeNotThreadSafeTest.java
+++ src/main/java/com/zlq/current/LongTypeNotThreadSafeTest.java
@@ -1,24 +1,39 @@
 package com.zlq.current;
 
+import org.junit.Test;
+
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * Created by liqing.zhan on 2016/7/12.
  */
 public class LongTypeNotThreadSafeTest {
     static long value = 0;
+    static AtomicLong atomicLong = new AtomicLong(0);
+    static LongAdder longAdder = new LongAdder();
 
-    public static void add() {
+    public static void addLong() {
         value++;
     }
 
-    public static void main(String args[]) throws InterruptedException {
+    public static void addAtomicLong() {
+        atomicLong.incrementAndGet();
+    }
+
+    public static void addLongAdder() {
+        longAdder.add(1);
+    }
+
+    @Test
+    public void t1() throws InterruptedException {
         int count = 5;
         while (count-- > 0) {
             new Thread(() -> {
                 int c = 10000;
                 while (c-- > 0) {
-                    LongTypeNotThreadSafeTest.add();
+                    LongTypeNotThreadSafeTest.addLong();
                 }
 
             }).start();
@@ -26,5 +41,39 @@ public class LongTypeNotThreadSafeTest {
 
         TimeUnit.SECONDS.sleep(5);
         System.out.println(value);
+    }
+
+    @Test
+    public void t2() throws InterruptedException {
+        int count = 5;
+        while (count-- > 0) {
+            new Thread(() -> {
+                int c = 10000;
+                while (c-- > 0) {
+                    LongTypeNotThreadSafeTest.addAtomicLong();
+                }
+
+            }).start();
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(atomicLong.get());
+    }
+
+    @Test
+    public void t3() throws InterruptedException {
+        int count = 5;
+        while (count-- > 0) {
+            new Thread(() -> {
+                int c = 10000;
+                while (c-- > 0) {
+                    LongTypeNotThreadSafeTest.addLongAdder();
+                }
+
+            }).start();
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(longAdder.longValue());
     }
 }
